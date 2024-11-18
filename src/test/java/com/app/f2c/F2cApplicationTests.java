334994619cd308fe59882b549c30677726ff4f5d
Mukund S Shelke <mukund.shelke@techbulls.com>
2024-11-18 15:06:49 +0530

    Initial Project file commit

---
+++ src/test/java/com/app/f2c/F2cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.f2c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F2cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
