56c15f316dd129c02a26c544f3e5579f9a8fb28b
MS1507 <shelkems91@gmail.com>
2025-01-19 08:35:50 +0530

    Initial commit for Speing Boot Config Server

---
+++ src/main/java/com/conf/SpringBootConfigServer/SpringBootConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.conf.SpringBootConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigServerApplication.class, args);
+	}
+
+}
